f4a039a00a4b38e96f1b484db26d12a668f1bfac
elyaperagov <44959297+elyaperagov@users.noreply.github.com>
2019-02-24 16:21:31 +0300

    слайдер и раздел "сервисы"

--- js/script.js
+++ js/script.js
@@ -30,11 +30,97 @@ linkMap.addEventListener("click", function (e) {
   popupMap.classList.add("modal-show");
 });
 
-priceoutput.value=pricecount.value
 
-var pagesList = document.querySelector(".catalog-pagination");
-var pageSwitch = document.querySelector(".pagination-item");
-pagesList.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  pageSwitch.classList.add("page-active");
+var deliveryButton = document.querySelector(".delivery-link");
+var guarantyButton = document.querySelector(".guaranty-link");
+var creditButton = document.querySelector(".credit-link");
+var deliveryServices = document.querySelector(".delivery-services");
+var guarantyServices = document.querySelector(".guaranty-services");
+var creditServices = document.querySelector(".credit-services");
+
+deliveryButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (guarantyButton.classList.contains("services-active")) {
+    guarantyButton.classList.remove("services-active");
+    guarantyButton.classList.add("services-not-active");
+  }
+
+  if (creditButton.classList.contains("services-active")) {
+    creditButton.classList.remove("services-active");
+    creditButton.classList.add("services-not-active");
+  }
+
+  if (guarantyServices.classList.contains("services-shown")) {
+    guarantyServices.classList.remove("services-shown");
+    guarantyServices.classList.add("services-hidden");
+  }
+
+  if (creditServices.classList.contains("services-shown")) {
+    creditServices.classList.remove("services-shown");
+    creditServices.classList.add("services-hidden");
+  }
+
+  deliveryButton.classList.remove("services-not-active");
+  deliveryButton.classList.add("services-active");
+  deliveryServices.classList.remove("services-hidden");
+  deliveryServices.classList.add("services-shown");
+});
+
+guarantyButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (deliveryButton.classList.contains("services-active")) {
+    deliveryButton.classList.remove("services-active");
+    deliveryButton.classList.add("services-not-active");
+  }
+
+  if (creditButton.classList.contains("services-active")) {
+    creditButton.classList.remove("services-active");
+    creditButton.classList.add("services-not-active");
+  }
+
+  if (deliveryServices.classList.contains("services-shown")) {
+    deliveryServices.classList.remove("services-shown");
+    deliveryServices.classList.add("services-hidden");
+  }
+
+  if (creditServices.classList.contains("services-shown")) {
+    creditServices.classList.remove("services-shown");
+    creditServices.classList.add("services-hidden");
+  }
+
+  guarantyButton.classList.remove("services-not-active");
+  guarantyButton.classList.add("services-active");
+  guarantyServices.classList.remove("services-hidden");
+  guarantyServices.classList.add("services-shown");
+});
+
+creditButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (deliveryButton.classList.contains("services-active")) {
+    deliveryButton.classList.remove("services-active");
+    deliveryButton.classList.add("services-not-active");
+  }
+
+  if (guarantyButton.classList.contains("services-active")) {
+    guarantyButton.classList.remove("services-active");
+    guarantyButton.classList.add("services-not-active");
+  }
+
+  if (deliveryServices.classList.contains("services-shown")) {
+    deliveryServices.classList.remove("services-shown");
+    deliveryServices.classList.add("services-hidden");
+  }
+
+  if (guarantyServices.classList.contains("services-shown")) {
+    guarantyServices.classList.remove("services-shown");
+    guarantyServices.classList.add("services-hidden");
+  }
+
+  creditButton.classList.remove("services-not-active");
+  creditButton.classList.add("services-active");
+  creditServices.classList.remove("services-hidden");
+  creditServices.classList.add("services-shown");
 });
